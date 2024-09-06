b20f07beac536552ef19cb010e39a64373ef5625
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-06 15:25:55 -0300

    A first way to use the functional interface function

---
+++ src/FunctionalInterfaceFunction.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FunctionalInterfaceFunction {
+    public static void main(String[] args) throws Exception {
+        FunctionalInterfaceFunction.forma1();
+    }
+
+    public static void forma1() {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+        Function<Integer, Integer> dobrar = numero -> numero * 2;
+
+        List numerosDobrados = numeros
+            .stream()
+            .map(dobrar)
+            .collect(Collectors.toList());
+
+        System.out.println(numerosDobrados);
+    }
+}
